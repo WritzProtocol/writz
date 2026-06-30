fc1402a63d31b7db09cd1a3a35009e334de964d2
Sebastián Salazar <sebastian.salazar.solano@gmail.com>
2026-06-29 21:58:56 -0600

    fix(relayer): restore Jest tsconfig after Bun migration

--- relayer/jest.config.js
+++ relayer/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
